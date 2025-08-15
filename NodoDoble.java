48277748a10d04374fd24843917383fc67cff6b9
AndreGoros <larigoju3@gmail.com>
2025-08-15 12:58:06 -0600

    Update NodoDoble.java

--- NodoDoble.java
+++ NodoDoble.java
@@ -9,7 +9,7 @@ public class NodoDoble <T>{
     private NodoDoble<T> anterior;
 
     public NodoDoble(T dato) {
-        this.dato = dato;
+        this.dato  = dato;
         this.anterior = null;
         this.siguiente = null;
     }
@@ -46,3 +46,4 @@ public class NodoDoble <T>{
     
     
 }
+
