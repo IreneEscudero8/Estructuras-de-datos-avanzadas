c2aa2b190878b81d440ad130b4451329a35cad24
IreneEscudero8 <150871591+IreneEscudero8@users.noreply.github.com>
2025-08-18 17:45:46 -0600

    Create ADTsException.java

---
+++ ADTsException.java
@@ -0,0 +1,14 @@
+/**
+ *  Represent wrong situations:
+ *     - a Null Pointer Reference,
+ *     - an Empty Collection,
+ *     - an Element Not Found.
+ */
+
+public class ADTsException extends RuntimeException {
+    /** Sets up this exception with an appropriate message.
+     */
+    public ADTsException(String collection) {
+        super ("> > > ADTsException:  " + collection);
+  }
+}
