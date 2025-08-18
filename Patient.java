9ce1af638f43aa807dbf290fc36d83fe33bebe16
IreneEscudero8 <150871591+IreneEscudero8@users.noreply.github.com>
2025-08-18 17:41:41 -0600

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,66 @@
+/*
+ * PROYECTO ESTRUCTURA DE DATOS AVANZADAS
+ * Equipo: Luis Fernando Reyes, Andre Gorostieta, Irene Escudero
+ * Clase que define un paciente por su nombre, id, prioridad y fecha de nacimiento
+ */
+
+public class Patient {
+    private String name;
+    private int id;
+    private int priority;
+    
+     public Patient() {
+       }
+   
+    public Patient(String name, int priority, int id) {
+        this.name = name;
+        this.priority = priority;
+        this.id=id;
+        }
+    
+    
+    public int getPriority() {
+       return priority;
+    }
+
+    public void setPriority (int num) {
+       priority = num;
+    }
+    
+     public String getName() {
+       return name;
+    }
+     
+    public void setName (String newName) {
+       name=newName;
+    }
+     
+      public int getId() {
+       return id;
+    }
+      
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Patient other = (Patient) obj;
+        return this.id == other.id;
+    }
+
+     
+    @Override
+    public String toString() {
+        String str;
+        str= "ID: " + getId()+ "\n NAME: " + getName() + "\n PRIORITY: " + getPriority(); 
+        return str;
+    }
+    
+}
