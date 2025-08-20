10f384dd034db8bba9f7135fd9670acd39a04fa6
IreneEscudero8 <150871591+IreneEscudero8@users.noreply.github.com>
2025-08-20 17:38:45 -0600

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -3,7 +3,8 @@ package practica0;
 
 /* * PROYECTO ESTRUCTURA DE DATOS AVANZADAS 
    * Equipo: Luis Fernando Reyes, Andre Gorostieta, Irene Escudero 
-   * Clase que define un paciente por su nombre, id, prioridad y fecha de nacimiento */ 
+   * Clase que define un paciente por su nombre, id y prioridad 
+*/ 
 public class Patient { 
     private String name; 
     private int id; 
@@ -54,4 +55,5 @@ public class Patient {
     public String toString() { 
         String str; str= "ID: " + getId()+ "\n NAME: " + getName() + "\n PRIORITY: " + getPriority(); return str; 
     } 
+
 } 
