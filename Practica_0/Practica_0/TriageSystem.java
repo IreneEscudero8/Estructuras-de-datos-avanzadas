f9ad5eb925ce339adf9a476ef1b93cb913c2f0a3
IreneEscudero8 <150871591+IreneEscudero8@users.noreply.github.com>
2025-08-20 19:00:00 -0600

    Comparar tiempos de ejecucion

--- Practica_0/Practica_0/TriageSystem.java
+++ Practica_0/Practica_0/TriageSystem.java
@@ -1,67 +1,83 @@
 /*
  * PROYECTO ESTRUCTURA DE DATOS AVANZADAS
  * Equipo: Luis Fernando Reyes, Andre Gorostieta, Irene Escudero
- * Clase clase tipo main que simula llegadas en tiempo real de 1000 pacientes con un tiempo entre arribos de 1 minuto 
+ * Clase clase tipo main que simula llegadas en tiempo real de 1000 pacientes y compara tiempos de ambas estructuras
 */
 
 import java.util.Random;
 
 public class TriageSystem {
 
-       public static void main(String[] args) {
-        DblyLnkSctr lista = new DblyLnkSctr();
+    public static void main(String[] args) {
+        int N = 1000;
         Random rand = new Random();
-       
-        boolean verificador;
-        Patient person;
-        LnrDblNd<Patient> nodo ;
-        int N = 1000, id;
         Patient[] pacientes = new Patient[N];
 
         // Generar pacientes prioridad aleatoria (1–10)
         for (int i = 0; i < N; i++) {
             int prioridad = rand.nextInt(10) + 1; // 1–10
-            pacientes[i] = new Patient("Paciente" + (i+1), prioridad, i+1);
-            System.out.println("CREADO: " + pacientes[i].toString());
+            pacientes[i] = new Patient("Paciente" + (i + 1), prioridad, i + 1);
         }
-        
-        //Medir insert
-        long startInsert = System.nanoTime();
-        for (int i = 0; i < N; i++) {
-            verificador=lista.addPatient(pacientes[i]);
-            System.out.println("Agregado: " + verificador);
-            try {
-                Thread.sleep(rand.nextInt(2)); // simular tiempo entre arribos (0-1 ms ~ 1 minuto simulado)
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        long endInsert = System.nanoTime();
-        System.out.println("Tiempo total Insert: " + (endInsert - startInsert)/1e6 + " ms");
 
-        //Medir search
-        long startSearch = System.nanoTime();
-        for (int i = 0; i < N; i++) {
-           id=rand.nextInt(N) + 1;
-           nodo=lista.findById(id); // buscar id aleatorio
-           if (nodo != null) {
-           System.out.println("Paciente encontrado: Id= " + id);
-           } else {
-             System.out.println("Paciente no encontrado");}
-        }
-        
-        long endSearch = System.nanoTime();
-        System.out.println("Tiempo total Search: " + (endSearch - startSearch)/1e6 + " ms");
+        // LISTA DOBLEMENTE LIGADA
+        DblyLnkSctr lista = new DblyLnkSctr();
 
-        // Eliminar todos los pacientes según prioridad (removeFirst) 
-        long startDelete = System.nanoTime();
+        // Insert
+        long startInsertLista = System.nanoTime();//contador de tiempo
+        for (int i = 0; i < N; i++) {
+            lista.addPatient(pacientes[i]);
+        }
+        long endInsertLista = System.nanoTime();//para el contador
+
+        // Search
+        long startSearchLista = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            int id = rand.nextInt(N) + 1;
+            lista.findById(id);
+        }
+        long endSearchLista = System.nanoTime();
+
+        // Delete
+        long startDeleteLista = System.nanoTime();
         while (!lista.isEmpty()) {
-            person = lista.removeFirst();
-            System.out.println("Atendido: Id=" + person.getId()); 
+            lista.removeFirst();
         }
-        long endDelete = System.nanoTime();
-        System.out.println("Tiempo total Delete (por prioridad): " + (endDelete - startDelete)/1e6 + " ms");
+        long endDeleteLista = System.nanoTime();
+
+        //ÁRBOL BINARIO DE BÚSQUEDA
+        PatientBST bst = new PatientBST();
+
+        // Insert
+        long startInsertBST = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            bst.insert(pacientes[i]);
+        }
+        long endInsertBST = System.nanoTime();
+
+        // Search(usamos prioridad aleatoria porque el BST busca por prioridad)
+        long startSearchBST = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            int pr = rand.nextInt(10) + 1; // prioridades entre 1 y 10
+            bst.search(pr);
+        }
+        long endSearchBST = System.nanoTime();
+
+        // Delete
+        long startDeleteBST = System.nanoTime();
+        for (int i = 1; i < N; i++) {
+            bst.removeFirst(); 
+        }
+        long endDeleteBST = System.nanoTime();
+
+        // RESULTADOS
+        System.out.println("\nRESULTADOS LISTA DOBLEMENTE LIGADA ");
+        System.out.println("Insert: " + (endInsertLista - startInsertLista) / 1e6 + " ms");// se divide para que sea mas facil leer ms
+        System.out.println("Search: " + (endSearchLista - startSearchLista) / 1e6 + " ms");
+        System.out.println("Delete: " + (endDeleteLista - startDeleteLista) / 1e6 + " ms");
+
+        System.out.println("\nRESULTADOS BST ");
+        System.out.println("Insert: " + (endInsertBST - startInsertBST) / 1e6 + " ms");
+        System.out.println("Search: " + (endSearchBST - startSearchBST) / 1e6 + " ms");
+        System.out.println("Delete: " + (endDeleteBST - startDeleteBST) / 1e6 + " ms");
     }
 }
-
-    
